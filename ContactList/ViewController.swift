2bc4bc74bc9eadb9faad276baf968b33513302c6
dimawko <sagyntkan@gmail.com>
2022-04-07 11:44:02 +0600

    Initial Commit

---
+++ ContactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Dinmukhammed Sagyntkan on 07.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
